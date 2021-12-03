18bbe18a33104bb12eb83e74f4f9c26021e1976b
mattbrannon <sh4rkbot@gmail.com>
2021-12-02 21:02:13 -0600

    add constants folder

---
+++ constants/contrast-ratios.js
@@ -0,0 +1,3 @@
+export const LOW = 3.0;
+export const AVG = 4.5;
+export const HIGH = 7.0;
---
+++ constants/unicodes.js
@@ -0,0 +1,4 @@
+export const REDX = '\u{274C}';
+export const WARNING = '\u{26A0}\u{FE0F}';
+export const OK = '\u{1F197}';
+export const CHECKMARK = '\u{2705}';
