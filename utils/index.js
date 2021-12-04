ece3ef59925bbd47279789ccc151c34bd09dcc33
mattbrannon <sh4rkbot@gmail.com>
2021-12-04 09:29:52 -0600

    rename variable, remove unused exports

--- utils/index.js
+++ utils/index.js
@@ -2,8 +2,6 @@ import { parseHSL } from '../lib/hsl.js';
 import { keepRgbInRange, parseRGB } from '../lib/rgb.js';
 import { COLORS } from '../constants/colors.js';
 
-const echo = (...args) => console.log(...args);
-
 const getDataType = (value) => {
   return Object.prototype.toString
     .call(value)
@@ -11,11 +9,11 @@ const getDataType = (value) => {
     .toLowerCase();
 };
 
-let isString = (value) => getDataType(value) === 'string';
-let isObject = (value) => getDataType(value) === 'object';
-let isPercent = (value) => isString(value) && value.endsWith('%');
+const isString = (value) => getDataType(value) === 'string';
+const isObject = (value) => getDataType(value) === 'object';
+const isPercent = (value) => isString(value) && value.endsWith('%');
 
-let isValidHex = (s) => {
+const isValidHex = (s) => {
   if (isString(s)) {
     const re = /^#?([0-9A-F]{3,4}|[0-9A-F]{6}|[0-9A-F]{8})$/i;
     return re.test(sanitize(s));
@@ -23,7 +21,7 @@ let isValidHex = (s) => {
   return false;
 };
 
-let isShortHex = (s) => isValidHex(s) && (s.length === 3 || s.length === 4);
+const isShortHex = (s) => isValidHex(s) && (s.length === 3 || s.length === 4);
 
 const isNamedColor = (str) => {
   if (isString(str)) {
@@ -34,9 +32,8 @@ const isNamedColor = (str) => {
   }
 };
 
-let percentToFloat = (num) => (isPercent(num) ? num.slice(0, -1) / 100 : undefined);
-
-let getColorSpaceFromString = (str) => isString(str) && str.slice(0, str.indexOf('('));
+const getColorSpaceFromString = (str) =>
+  isString(str) && str.slice(0, str.indexOf('('));
 
 const sanitize = (str) => {
   return str
@@ -71,36 +68,27 @@ const convertAlpha = (alpha) => {
   return result > 1 ? 1 : result < 0 ? 0 : result;
 };
 
-const getColorType = (color) => {
-  const dataType = getDataType(color);
-  if (dataType === 'string') {
-    if (!isValidHex(color)) {
-      return getColorSpaceFromString(color);
-    }
-    return 'hex';
-  }
-  else if (dataType === 'object') {
-    return Object.keys(color).join('');
-  }
-};
-
 const parseString = (str) => {
-  let format = getColorSpaceFromString(str);
+  let colorSpace = getColorSpaceFromString(str);
   const rgb = [ 'rgb', 'rgba' ];
   const hsl = [ 'hsl', 'hsla' ];
 
-  const color = rgb.includes(format) ? parseRGB(str) : hsl.includes(format) ? parseHSL(str) : null;
+  const color = rgb.includes(colorSpace)
+    ? parseRGB(str)
+    : hsl.includes(colorSpace)
+    ? parseHSL(str)
+    : null;
 
-  format = getColorSpaceFromString(color.css());
+  colorSpace = getColorSpaceFromString(color.css());
   const values = color.array();
 
-  return { format, values };
+  return { colorSpace, values };
 };
 
 const toObject = (str) => {
-  const { values, format } = parseString(str);
+  const { values, colorSpace } = parseString(str);
   return values.reduce((acc, val, i) => {
-    const key = format[i];
+    const key = colorSpace[i];
     acc[key] = val;
     return acc;
   }, {});
@@ -109,33 +97,32 @@ const toObject = (str) => {
 const toString = (object) => {
   const rgb = [ 'rgb', 'rgba' ];
   const hsl = [ 'hsl', 'hsla' ];
-  const format = Object.keys(object).join('');
+  const colorSpace = Object.keys(object).join('');
 
-  const setHslString = (num, i) => (i === 0 ? `${num}deg` : i < 3 ? `${num}%` : num);
+  const setHslString = (num, i) =>
+    i === 0 ? `${num}deg` : i < 3 ? `${num}%` : num;
 
   const setRgbString = (num, i) => (i < 3 ? keepRgbInRange(num) : num);
 
-  const setter = rgb.includes(format) ? setRgbString : hsl.includes(format) ? setHslString : null;
+  const setter = rgb.includes(colorSpace)
+    ? setRgbString
+    : hsl.includes(colorSpace)
+    ? setHslString
+    : null;
   const values = Object.values(object).map(setter);
-  return `${format}(${values.join(', ')})`;
+  return `${colorSpace}(${values.join(', ')})`;
 };
 
 export {
-  echo,
   convertAlpha,
-  getDataType,
   sanitize,
-  isPercent,
   stripPercent,
-  percentToFloat,
   getColorSpaceFromString,
-  getColorType,
   toFloat,
   toObject,
   isObject,
   toString,
   isValidHex,
   isShortHex,
-  parseRGB,
   isNamedColor,
 };
