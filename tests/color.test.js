7c7f79fae50106dae7b11d0cbba832455adba2d6
mattbrannon <sh4rkbot@gmail.com>
2022-04-03 19:56:40 -0500

    update tests

--- tests/color.test.js
+++ tests/color.test.js
@@ -1,86 +1,87 @@
 /* eslint-disable no-undef */
 
-import { expect } from 'chai';
-import Color from '../main.js';
-import { isValidHex } from '../utils/index.js';
+import { expect } from "chai";
+import Color from "../src/color.js";
+import { isValidHex } from "../utils/index.js";
 
-describe('Color class', () => {
-  const red1 = new Color('red');
-  const red2 = new Color('#ff0000');
-  const red3 = new Color('rgb(255, 0, 0)');
-  const red4 = new Color('hsl(0, 100%, 50%)');
+describe("Color class", () => {
+  const red1 = new Color("red");
+  const red2 = new Color("#ff0000");
+  const red3 = new Color("rgb(255, 0, 0)");
+  const red4 = new Color("hsl(0, 100%, 50%)");
 
   const methods = [
-    'css',
-    'array',
-    'object',
-    'shades',
-    'tints',
-    'faded',
-    'vibrant',
+    "css",
+    "array",
+    "object",
+    "shades",
+    "tints",
+    "faded",
+    "vibrant",
+    "random",
   ];
 
-  describe('static methods', () => {
-    describe('random', () => {
-      it('should exist', () => {
+  describe("static methods", () => {
+    describe("random", () => {
+      it("should exist", () => {
         expect(Color.random()).to.exist;
       });
-      it('should be a function', () => {
-        expect(typeof Color.random).to.equal('function');
+      it("should be a function", () => {
+        expect(typeof Color.random).to.equal("function");
       });
-      it('should return a valid hex string', () => {
+      it("should return a valid hex string", () => {
         expect(isValidHex(Color.random())).to.be.true;
       });
-      it('should return a random hex string', () => {
+      it("should return a random hex string", () => {
         const hex1 = Color.random();
         const hex2 = Color.random();
         expect(hex1).to.not.equal(hex2);
       });
     });
-    describe('constrast', () => {
-      it('should exist', () => {
+    describe("constrast", () => {
+      it("should exist", () => {
         expect(Color.contrast).to.exist;
       });
-      it('should be a function', () => {
-        expect(typeof Color.contrast).to.equal('function');
+      it("should be a function", () => {
+        expect(typeof Color.contrast).to.equal("function");
       });
-      it('should return a number', () => {
-        const ratio = Color.contrast('blue', 'green');
-        expect(typeof ratio).to.equal('number');
+      it("should return a number", () => {
+        const ratio = Color.contrast("blue", "green");
+        expect(typeof ratio).to.equal("number");
       });
-      it('should return the correct contrast ratio', () => {
-        const ratio = Color.contrast('blue', 'green');
+      it("should return the correct contrast ratio", () => {
+        const ratio = Color.contrast("blue", "green");
         expect(ratio).to.equal(1.67);
       });
     });
   });
 
-  describe('properties', () => {
-    describe('hex', () => {
-      it('should have a hex property', () => {
+  describe("properties", () => {
+    describe("hex", () => {
+      it("should have a hex property", () => {
         expect(red1.hex).to.exist;
       });
-      it('should have methods attached', () => {
+      it("should have methods attached", () => {
         Object.keys(red1.hex).forEach((property) => {
           expect(methods.includes(property)).to.be.true;
         });
       });
     });
-    describe('rgb', () => {
-      it('should have an rgb property', () => {
+    describe("rgb", () => {
+      it("should have an rgb property", () => {
         expect(red2.rgb).to.exist;
       });
-      it('should have methods attached', () => {
+      it("should have methods attached", () => {
         Object.keys(red2.rgb).forEach((property) => {
           expect(methods.includes(property)).to.be.true;
         });
       });
     });
-    describe('hsl', () => {
-      it('should have an hsl property', () => {
+    describe("hsl", () => {
+      it("should have an hsl property", () => {
         expect(red3.hsl).to.exist;
       });
-      it('should have methods attached', () => {
+      it("should have methods attached", () => {
         Object.keys(red3.hsl).forEach((property) => {
           expect(methods.includes(property)).to.be.true;
         });
@@ -88,17 +89,17 @@ describe('Color class', () => {
     });
   });
 
-  describe('methods', () => {
-    describe('set', () => {
-      const color1 = new Color('blue');
+  describe("methods", () => {
+    describe("set", () => {
+      const color1 = new Color("blue");
       const color2 = color1.set.red(100);
-      it('should return an instance of color', () => {
+      it("should return an instance of color", () => {
         expect(color2).to.be.instanceOf(Color);
       });
-      it('should return a new instance of color', () => {
+      it("should return a new instance of color", () => {
         expect(Object.is(color1, color2)).to.be.false;
       });
-      it('should not modify the original color', () => {
+      it("should not modify the original color", () => {
         expect(color2.hex.css()).to.not.equal(color1.hex.css());
       });
     });
--- tests/convert.test.js
+++ tests/convert.test.js
@@ -1,76 +1,76 @@
 /* eslint-disable no-undef */
 
-import { expect } from "chai";
-import convert from "../converter/index.js";
+import { expect } from 'chai';
+import convert from '../converter/index.js';
 
-describe("Color conversion tests", () => {
-  const hex = "#ff00ff";
+describe('Color conversion tests', () => {
+  const hex = '#ff00ff';
   const rgb = { r: 255, g: 0, b: 255 };
   const hsl = { h: 300, s: 100, l: 50 };
   const hsv = { h: 300, s: 100, v: 100 };
 
-  describe("hex2rgb", () => {
-    it("should convert hex to rgb", () => {
+  describe('hex2rgb', () => {
+    it('should convert hex to rgb', () => {
       expect(convert.hex2rgb(hex)).to.deep.equal(rgb);
     });
   });
-  describe("hex2hsl", () => {
-    it("should convert hex to hsl", () => {
+  describe('hex2hsl', () => {
+    it('should convert hex to hsl', () => {
       expect(convert.hex2hsl(hex)).to.deep.equal(hsl);
     });
   });
-  describe("hex2hsv", () => {
-    it("should convert hex to hsv", () => {
+  describe('hex2hsv', () => {
+    it('should convert hex to hsv', () => {
       expect(convert.hex2hsv(hex)).to.deep.equal(hsv);
     });
   });
 
-  describe("rgb2hex", () => {
-    it("should convert rgb to hex", () => {
+  describe('rgb2hex', () => {
+    it('should convert rgb to hex', () => {
       expect(convert.rgb2hex(rgb)).to.equal(hex);
     });
   });
-  describe("rgb2hsl", () => {
-    it("should convert rgb to hsl", () => {
+  describe('rgb2hsl', () => {
+    it('should convert rgb to hsl', () => {
       expect(convert.rgb2hsl(rgb)).to.deep.equal(hsl);
     });
   });
-  describe("rgb2hsv", () => {
-    it("should convert rgb to hsv", () => {
+  describe('rgb2hsv', () => {
+    it('should convert rgb to hsv', () => {
       expect(convert.rgb2hsv(rgb)).to.deep.equal(hsv);
     });
   });
 
-  describe("hsl2rgb", () => {
-    it("should convert hsl to rgb", () => {
+  describe('hsl2rgb', () => {
+    it('should convert hsl to rgb', () => {
       expect(convert.hsl2rgb(hsl)).to.deep.equal(rgb);
     });
   });
 
-  describe("hsl2hex", () => {
-    it("should convert hsl to hex", () => {
+  describe('hsl2hex', () => {
+    it('should convert hsl to hex', () => {
       expect(convert.hsl2hex(hsl)).to.equal(hex);
     });
   });
 
-  describe("hsl2hsv", () => {
-    it("should convert hsl to hsv", () => {
+  describe('hsl2hsv', () => {
+    it('should convert hsl to hsv', () => {
       expect(convert.hsl2hsv(hsl)).to.deep.equal(hsv);
     });
   });
 
-  describe("hsv2hex", () => {
-    it("should convert hsv to hex", () => {
+  describe('hsv2hex', () => {
+    it('should convert hsv to hex', () => {
       expect(convert.hsv2hex(hsv)).to.equal(hex);
     });
   });
-  describe("hsv2rgb", () => {
-    it("should convert hsv to rgb", () => {
+  describe('hsv2rgb', () => {
+    it('should convert hsv to rgb', () => {
       expect(convert.hsv2rgb(hsv)).to.deep.equal(rgb);
     });
   });
-  describe("hsv2hsl", () => {
-    it("should convert hsv to hsl", () => {
+  describe('hsv2hsl', () => {
+    it('should convert hsv to hsl', () => {
       expect(convert.hsv2hsl(hsv)).to.deep.equal(hsl);
     });
   });
