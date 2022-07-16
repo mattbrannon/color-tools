cfd4bf68ff7eb96f9a73d20d093f8b153dee9720
mattbrannon <sh4rkbot@gmail.com>
2022-07-16 10:56:54 -0500

    update build configs

--- rollup.config.js
+++ rollup.config.js
@@ -3,18 +3,6 @@ import commonjs from '@rollup/plugin-commonjs';
 import pkg from './package.json';
 
 export default [
-  // {
-  //   input: 'dist/lib/index.js',
-  //   output: {
-  //     name: 'colorTools',
-  //     file: pkg.browser,
-  //     format: 'umd',
-  //   },
-  //   plugins: [
-  //     resolve(),
-  //     commonjs(),
-  //   ],
-  // },
   {
     plugins: [ resolve(), commonjs() ],
     input: 'dist/lib/index.js',
