864f5990a998f267e9a4284db087eaf8c48bd604
mattbrannon <sh4rkbot@gmail.com>
2022-07-16 00:21:39 -0500

    remove export for internal modules

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-export { parseHsl } from './hsl-new.js';
-export { parseHex } from './hex-new.js';
-export { parseRgb } from './rgb-new.js';
 export { Color } from './color.js';
 export { Theme } from './theme.js';
 export { convert } from './convert.js';
-export { utils } from './utils.js';
