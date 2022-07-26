be54d7fbebd0b19b9e86af6a7f26ead5a2a0d0bc
mattbrannon <sh4rkbot@gmail.com>
2022-07-26 16:50:48 -0500

    remove exports and unused functions

--- src/angles.ts
+++ src/angles.ts
@@ -1,29 +1,29 @@
 export const radianToDegree = (rad: number) => {
-  return Math.round(rad * (180 / Math.PI) * 10000) / 10000;
+  return Math.round(rad * (180 / Math.PI) * 100) / 100;
 };
 
-const radianToTurn = (rad: number) => {
+export const radianToTurn = (rad: number) => {
   return Math.round((rad / (Math.PI * 2)) * 10000) / 10000;
 };
 
-const radianToGradian = (rad: number) => {
-  return Math.round(rad * (200 / Math.PI) * 10000) / 10000;
+export const radianToGradian = (rad: number) => {
+  return Math.round(rad * (200 / Math.PI) * 100) / 100;
 };
 
-const degreeToRadian = (deg: number) => {
+export const degreeToRadian = (deg: number) => {
   return Math.round(deg * (Math.PI / 180) * 10000) / 10000;
 };
 
-const degreeToTurn = (deg: number) => {
+export const degreeToTurn = (deg: number) => {
   return Math.round((deg / 360) * 10000) / 10000;
 };
 
-const degreeToGradian = (deg: any) => {
+export const degreeToGradian = (deg: number) => {
   const rad = degreeToRadian(deg);
   return radianToGradian(rad);
 };
 
-const turnToRadian = (turn: number) => {
+export const turnToRadian = (turn: number) => {
   return Math.round(turn * (Math.PI * 2) * 10000) / 10000;
 };
 
@@ -31,126 +31,51 @@ export const turnToDegree = (turn: number) => {
   return Math.round(turn * 360 * 10000) / 10000;
 };
 
-const turnToGradian = (turn: any) => {
+export const turnToGradian = (turn: number) => {
   const rad = turnToRadian(turn);
   return radianToGradian(rad);
 };
 
-const gradianToRadian = (grad: number) => {
+export const gradianToRadian = (grad: number) => {
   return Math.round(grad * (Math.PI / 200) * 10000) / 10000;
 };
 
-export const gradianToDegree = (grad: any) => {
+export const gradianToDegree = (grad: number) => {
   const rad = gradianToRadian(grad);
   return radianToDegree(rad);
 };
 
-const gradianToTurn = (grad: any) => {
+export const gradianToTurn = (grad: number) => {
   const rad = gradianToRadian(grad);
   return radianToTurn(rad);
 };
 
 export const identity = (n: any) => n;
 
-export const angle = {
-  gradianToDegree,
-  gradianToRadian,
-  gradianToTurn,
-  radianToDegree,
-  radianToGradian,
-  radianToTurn,
-  degreeToRadian,
-  degreeToGradian,
-  degreeToTurn,
-  turnToRadian,
-  turnToGradian,
-  turnToDegree,
-  identity,
+export const gradian = {
+  toDegree: (n: number) => gradianToDegree(n),
+  toRadian: (n: number) => gradianToRadian(n),
+  toTurn: (n: number) => gradianToTurn(n),
+  toGradian: (n: number) => identity(n),
 };
 
-export const gradian = (n: number) => {
-  return {
-    toDegree: gradianToDegree(n),
-    toRadian: gradianToRadian(n),
-    toTurn: gradianToTurn(n),
-    toGradian: identity(n)
-  };
+export const radian = {
+  toDegree: (n: number) => radianToDegree(n),
+  toGradian: (n: number) => radianToGradian(n),
+  toTurn: (n: number) => radianToTurn(n),
+  toRadian: (n: number) => identity(n),
 };
 
-export const radian = (n: number) => {
-  return {
-    toDegree: radianToDegree(n),
-    toGradian: radianToGradian(n),
-    toTurn: radianToTurn(n),
-    toRadian: identity(n),
-  };
+export const degree = {
+  toRadian: (n: number) => degreeToRadian(n),
+  toGradian: (n: number) => degreeToGradian(n),
+  toTurn: (n: number) => degreeToTurn(n),
+  toDegree: (n: number) => identity(n),
 };
 
-export const degree = (n: number) => {
-  return {
-    toRadian: degreeToRadian(n),
-    toGradian: degreeToGradian(n),
-    toTurn: degreeToTurn(n),
-    toDegree: identity(n)
-  };
+export const turn = {
+  toRadian: (n: number) => turnToRadian(n),
+  toGradian: (n: number) => turnToGradian(n),
+  toDegree: (n: number) => turnToDegree(n),
+  toTurn: (n: number) => identity(n),
 };
-
-export const turn = (n: number) => {
-  return {
-    toRadian: turnToRadian(n),
-    toGradian: turnToGradian(n),
-    toDegree: turnToDegree(n),
-    toTurn: identity(n)
-  };
-};
-
-// export class Angle {
-//   constructor() {
-//     return {
-//       gradian: {
-//         toDegree(n: number) {
-//           return gradianToDegree(n);
-//         },
-//         toRadian(n: number) {
-//           return gradianToRadian(n);
-//         },
-//         toTurn(n: number) {
-//           return gradianToTurn(n);
-//         },
-//       },
-//       radian: {
-//         toDegree(n: number) {
-//           return radianToDegree(n);
-//         },
-//         toGradian(n: number) {
-//           return radianToGradian(n);
-//         },
-//         toTurn(n: number) {
-//           return radianToTurn(n);
-//         },
-//       },
-//       degree: {
-//         toRadian(n: number) {
-//           return degreeToRadian(n);
-//         },
-//         toGradian(n: number) {
-//           return degreeToGradian(n);
-//         },
-//         toTurn(n: number) {
-//           return degreeToTurn(n);
-//         },
-//       },
-//       turn: {
-//         toRadian(n: number) {
-//           return turnToRadian(n);
-//         },
-//         toGradian(n: number) {
-//           return turnToGradian(n);
-//         },
-//         toDegree(n: number) {
-//           return turnToDegree(n);
-//         },
-//       },
-//     };
-//   }
-// }
--- src/hex-new.ts
+++ src/hex-new.ts
@@ -1,33 +1,34 @@
 import { isHex, removeHash, addHash } from './utils';
+import { RgbHex } from './interfaces';
 
-export const isShort = (s: string) => {
+const isShort = (s: string) => {
   return isHex(s) && removeHash(s).length <= 4;
 };
 
-export const canBeShort = (s: string) => {
-  return Array.from(s).every((char, i, coll) => {
-    return i % 2 === 0 ? char === coll[i + 1] : char === coll[i - 1];
-  });
-};
+// const canBeShort = (s: string) => {
+//   return Array.from(s).every((char, i, coll) => {
+//     return i % 2 === 0 ? char === coll[i + 1] : char === coll[i - 1];
+//   });
+// };
 
-export const isLong = (s: string) => {
-  return isHex(s) && removeHash(s).length >= 6;
-};
+// const isLong = (s: string) => {
+//   return isHex(s) && removeHash(s).length >= 6;
+// };
 
-export const hasAlpha = (s: string) => {
-  return isLong(s) && removeHash(s).length > 6;
-};
+// const hasAlpha = (s: string) => {
+//   return isLong(s) && removeHash(s).length > 6;
+// };
 
-export const makeShort = (s: string) => {
-  if (isLong(s)) {
-    return addHash(
-      Array.from(removeHash(s))
-        .filter((_, i) => i % 2 === 0)
-        .join('')
-    );
-  }
-  return addHash(s);
-};
+// const makeShort = (s: string) => {
+//   if (isLong(s)) {
+//     return addHash(
+//       Array.from(removeHash(s))
+//         .filter((_, i) => i % 2 === 0)
+//         .join('')
+//     );
+//   }
+//   return addHash(s);
+// };
 
 export const makeLong = (s: string) => {
   if (isShort(s)) {
@@ -44,40 +45,23 @@ export const parseHex = (s: string) => {
   const hex = removeHash(makeLong(s));
 
   const array = Array.from(hex)
-    .map((char, i, coll) => (i % 2 === 0 ? char + coll[i + 1] : null))
+    .map((char, i, coll) => {
+      return i % 2 === 0 ? char + coll[i + 1] : '';
+    })
     .filter((v) => v);
 
-  const object = array.reduce((acc, value, i) => {
+  const object: RgbHex = array.reduce((acc, value, i) => {
     const keys = 'rgb';
-    const key = i === 3 ? 'a' : keys[i];
-    acc[key] = value;
+    const key: string = i === 3 ? 'a' : keys[i];
+    // if (value) {
+    acc[key as keyof RgbHex] = value;
+    // }
     return acc;
-  }, {});
+  }, {} as RgbHex);
 
   return {
-    /**
-     *
-     * @returns an array with the hexadecimal bits of color split into their rgb counterparts
-     */
     array: (): string[] => array,
-    /**
-     *
-     * @returns an object with the hex string broken up into its rgb counterparts
-     */
     object: (): {} => object,
-    /**
-     *
-     * @returns a hexadecimal css color
-     */
     css: (): string => addHash(hex),
   };
 };
-
-// export const parseHex = (s: string) => {
-//   const longHex = makeLong(s);
-//   const hex = hasAlpha(longHex) ? longHex.slice(0, 7) : longHex;
-//   return {
-//     hex,
-//     alpha: hasAlpha(longHex) ? longHex : null,
-//   };
-// };
