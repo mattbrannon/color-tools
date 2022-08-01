917e63b1960cac9767af50b3c8e216bde2ecc525
mattbrannon <sh4rkbot@gmail.com>
2022-08-01 09:59:38 -0500

    add terser plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,26 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import pkg from './package.json';
+import { terser } from 'rollup-plugin-terser';
+
+const toCamelCase = (s) => {
+  const re = /(-[a-z]){1}/g;
+  return s.toLocaleLowerCase().replace(re, (s) => s.slice(1).toUpperCase());
+};
 
 export default [
   {
-    plugins: [ resolve(), commonjs() ],
+    plugins: [ commonjs(), resolve() ],
     input: 'dist/lib/index.js',
     output: [
-      { name: 'colorTools', file: pkg.browser, format: 'umd' },
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' },
+      {
+        name: toCamelCase(pkg.name),
+        file: pkg.browser,
+        format: 'umd',
+        plugins: [terser()],
+      },
+      { file: pkg.main, format: 'cjs', plugins: [terser()] },
+      { file: pkg.module, format: 'esm', plugins: [terser()] },
     ],
   },
 ];
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-export { Color } from './color';
-export { Theme } from './theme';
+// export { Color } from './color';
+export { Theme as Color } from './theme';
+export { suggest } from './contrast-helper';
 export { convert } from './convert';
-
-// TODO: find a better way of testing
-
-// export { utils } from './utils.js';
-// export { parseHsl } from './hsl-new.js';
-// export { parseHex } from './hex-new.js';
-// export { parseRgb } from './rgb-new.js';
+export { parseHex } from './hex-new.js';
+export { parseHsl } from './hsl-new.js';
+export { parseRgb } from './rgb-new.js';
+export { isNamedColor } from './utils';
+export * from './angles';
