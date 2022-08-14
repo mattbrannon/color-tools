84a6112d67d66b442ea658e0ed9bb5e3c987baf6
mattbrannon <sh4rkbot@gmail.com>
2022-08-13 22:07:01 -0500

    update exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-// export { Color } from './color';
-export { Theme as Color } from './theme';
-export { suggest } from './contrast-helper';
+export { Color } from './color';
+export { Theme } from './theme';
 export { convert } from './convert';
-export { parseHex } from './hex-new.js';
-export { parseHsl } from './hsl-new.js';
-export { parseRgb } from './rgb-new.js';
-export { isNamedColor } from './utils';
-export * from './angles';
