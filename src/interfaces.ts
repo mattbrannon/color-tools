54e3ca88aa8227303ae10a257ab82cf7a3d168a1
mattbrannon <sh4rkbot@gmail.com>
2022-08-13 21:39:42 -0500

    update types to include valid number ranges

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -8,13 +8,6 @@ export interface RgbColor {
   a?: number;
 }
 
-export interface HslColor {
-  h?: number;
-  s?: number;
-  l?: number;
-  a?: number;
-}
-
 export interface HsvColor {
   h: number;
   s: number;
@@ -54,3 +47,44 @@ export interface ColorGenerator {
 
 export type Limit = number;
 export type Step = number;
+
+export type InputArray = [any, any, any, any?];
+export type ColorArray = [number, number, number, number?];
+
+type Enumerate<N extends number, Acc extends number[] = []> = Acc['length'] extends N
+  ? Acc[number]
+  : Enumerate<N, [...Acc, Acc['length']]>;
+
+type Range<F extends number, T extends number> = Exclude<Enumerate<T>, Enumerate<F>>;
+
+export type RgbValues = Range<0, 256>;
+export type HueValues = Range<0, 361>;
+export type PercentValues = Range<0, 101>;
+export type AlphaValues = Range<0, 2>;
+
+export type RgbArrayValues = [RgbValues, RgbValues, RgbValues, AlphaValues?];
+export type HslArray = [HueValues, PercentValues, PercentValues, AlphaValues?];
+
+export type HslObject = {
+  h: HueValues;
+  s: PercentValues;
+  l: PercentValues;
+  a?: AlphaValues;
+};
+
+// export enum HslValues {
+//   hue = 0 | 360,
+//   saturation = 0 || 100,
+//   lightness = 0 || 100,
+// }
+
+// type RGBColor = number & { _type_: 'RGBColor' };
+// type Hue = number & { _type_: 'Hue' };
+
+// export const validateHue = (value: number): HueValues => {
+//   if (value < 0 || value > 255) {
+//     throw new Error(`The value ${value} is not a valid color`);
+//   }
+
+//   return value as HueValues;
+// };
