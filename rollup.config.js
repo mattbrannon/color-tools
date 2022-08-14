bb8ab5b8278b20c6cd619750d4f2419ae34d7833
mattbrannon <sh4rkbot@gmail.com>
2022-08-13 21:55:24 -0500

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import pkg from './package.json';
-import { terser } from 'rollup-plugin-terser';
-
-const toCamelCase = (s) => {
-  const re = /(-[a-z]){1}/g;
-  return s.toLocaleLowerCase().replace(re, (s) => s.slice(1).toUpperCase());
-};
+// import { terser } from 'rollup-plugin-terser';
 
 export default [
   {
@@ -14,13 +9,12 @@ export default [
     input: 'dist/lib/index.js',
     output: [
       {
-        name: toCamelCase(pkg.name),
+        name: 'colorTools',
         file: pkg.browser,
         format: 'umd',
-        plugins: [terser()],
       },
-      { file: pkg.main, format: 'cjs', plugins: [terser()] },
-      { file: pkg.module, format: 'esm', plugins: [terser()] },
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'esm' },
     ],
   },
 ];
